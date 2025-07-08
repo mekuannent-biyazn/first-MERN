ce6da38da181bfa7d012173935081e004014df62
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2025-07-08 11:51:19 +0300

    full MERN stack

---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,10 @@
+// const registerUser = async (req, res) => {
+//   const { name, email, password, pic } = req.bode;
+
+//   res.json({
+//     name,
+//     email,
+//   });
+// };
+
+// module.export = { registerUser };
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,27 @@
 const express = require("express");
 const notes = require("./data/notes");
 const dotenv = require("dotenv");
+// // const { connect } = require("mongoose");
 
-const app = express();
 dotenv.config();
+// const connectDB = require("./config/db");
+// connectDB();
+const app = express();
+// const noteRoutes = require("./routers/noteRoutes");
+// app.use("/api/notes", noteRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running..");
 });
 
 app.get("/api/notes", (req, res) => {
-  res.send(notes);
+  res.json(notes);
 });
 
-app.get("/api/notes:id", (req, res) => {
+app.get("/api/notes/:id", (req, res) => {
   const note = notes.find((n) => n._id === req.params.id);
 
   res.send(note);
-  // console.log(req.params);
 });
 
 const PORT = process.env.PORT || 5000;
