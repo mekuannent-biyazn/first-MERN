9fe9382b30b993d2965c8ff4c58f3eca201eb045
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2025-09-15 22:00:30 +0300

    update package.json file

--- backend/server.js
+++ backend/server.js
@@ -17,17 +17,17 @@ app.use("/api/notes", noteRoutes);
 
 // ........................Deployment........................
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
-  });
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running");
-  });
-}
+//   app.get("/*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
+//   });
+// } else {
+//   app.get("/", (req, res) => {
+//     res.send("API is running");
+//   });
+// }
 
 // ........................Deployment........................
 
