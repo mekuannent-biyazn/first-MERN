e44f0e4d39c652bfb8f3c2078580a3f33f863c31
mekuannent-biyazn <mekuannentbiyazn@gmail.com>
2026-01-27 10:43:09 +0300

    Now Fix DB conctivity error

--- backend/server.js
+++ backend/server.js
@@ -4,42 +4,15 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routers/userRouters");
 const noteRoutes = require("./routers/noteRoutes");
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
-const path = require("path");
-const cors = require("cors");
 
 const app = express();
 dotenv.config();
 connectDB();
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: "https://first-mern-20fu.onrender.com", // your frontend URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  })
-);
 
 app.use("/api/users", userRoutes);
 app.use("/api/notes", noteRoutes);
-app.get("/", (req, res) => {
-  res.send("Api is running");
-});
-
-// ........................Deployment........................
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-//   app.get("/*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running");
-//   });
-// }
-
-// ........................Deployment........................
 
 app.use(notFound);
 app.use(errorHandler);
